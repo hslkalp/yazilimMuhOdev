85b8ca8ef07480dcb6a0ffd06b0a86a24d303e82
hslkalp <hslkalp@gmail.com>
2022-04-27 23:19:09 -0700

    Update main

--- src/main/java/yazilimMuh/App.java
+++ src/main/java/yazilimMuh/App.java
@@ -13,6 +13,9 @@ import java.util.Random;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.port;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -28,6 +31,11 @@ public class App {
 
         Logger logger = LogManager.getLogger(App.class);
         logger.error("Custom error message.");
+
+
+        int port = Integer.parseInt(System.getenv("PORT"));
+        port(port);
+        logger.error("Current port number is "+ port);
 
         get("/compute",
                 (rq, rs) -> {
